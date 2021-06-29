9158b1b11010d24343d106cca9b5662a7154c7b7
nicolasquinchia <nicolasquinchia@gmail.com>
2021-06-29 00:14:45 -0500

    JS Advanced, Closure, var that contains function calls

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+/**
+ * Function that displays a greeting
+ * @param {String} fullName Name to display in the gretting
+ */
+function welcomeMessage(fullName) {
+    alert("Welcome " + fullName);
+}
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
